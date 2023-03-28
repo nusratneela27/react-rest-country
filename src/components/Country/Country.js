0dce1546ff60af5b076030c3d92996694fa46506
neela <nusratneela27@gmail.com>
2023-03-29 00:38:16 +0600

    first commit

---
+++ src/components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+   const {area,region,population,name,flags} = props.country;
+    return (
+        <div className='country bg-warning'>
+            <h2>Country Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p><small>Region: {region}</small></p>
+            <p><small>Area: {area}</small></p>
+        </div>
+    );
+};
+
+export default Country;
